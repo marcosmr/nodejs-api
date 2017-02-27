609e1f551e140fbc9ef301aa181799780556471e
josueabenamar <josueabenamar.dev@gmail.com>
2017-02-27 14:35:43 -0600

    mongo aggregate populate

--- components/database_mongodb.js
+++ components/database_mongodb.js
@@ -166,7 +166,14 @@ function get(config, errors, logger)
 				return;
 			}
 
-			callback(collection);
+			if(query.population)
+			{
+				Model.populate(collection, query.population, callback);
+			}
+			else
+			{
+				callback(collection);
+			}
 		});
 	}
 
