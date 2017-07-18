397a92f5279d22cc39d5a42637f6a4a215c21bf8
josueabenamar <josueabenamar.dev@gmail.com>
2017-07-18 17:22:52 -0500

    conekta 2 create account

--- components/payment_conekta.js
+++ components/payment_conekta.js
@@ -3,6 +3,7 @@ function get(config, errors, logger)
 	var component = {};
 
 	var apikey = config.apikey;
+	var version = config.version;
 
 
 	if(!apikey)
@@ -11,165 +12,320 @@ function get(config, errors, logger)
 		process.exit(1);
 	}
 
+	if(!version)
+	{
+		version = 1;
+	}
+
 	var key = new Buffer(apikey).toString('base64');
 
 	var https = require('https');
 
 
-	var createAccount = function(params, res, callback, callback_error)
+	if(version == 1)
 	{
-		var name = params.name;
-		var email = params.email;
-		var token = params.token;
-
-		var options =
+		var createAccount = function(params, res, callback, callback_error)
 		{
-			path: '/customers',
-			method: 'POST',
-			data:
+			var name = params.name;
+			var email = params.email;
+			var token = params.token;
+
+			var options =
 			{
-				name: name,
-				email: email,
-				cards: [token]
-			}
-		};
-
-		sendRequest(options, res, callback, callback_error);
-	}
-
-	var updateAccount = function(params, res, callback, callback_error)
-	{
-		var conekta = params.conekta;
-		var card = params.card;
-		var token = params.token;
-
-		var options =
-		{
-			path: '/customers/' + conekta + '/cards/' + card,
-			method: 'PUT',
-			data:
-			{
-				token: token
-			}
-		}
-
-		sendRequest(options, res, callback, callback_error);
-	}
-
-	var pay = function(params, res, callback, callback_error)
-	{
-		var card = params.card;
-		var total = params.total;
-		var currency = params.currency;
-		var description = params.description;
-		var name = params.name;
-		var email = params.email;
-		var phone = params.phone;
-		var items = params.items;
-
-		var options =
-		{
-			path: '/charges',
-			method: 'POST',
-			data:
-			{
-				card: card,
-				amount: total,
-				currency: currency,
-				description: description,
-				details:
+				path: '/customers',
+				method: 'POST',
+				data:
 				{
 					name: name,
 					email: email,
-					phone: phone,
-					line_items: items
+					cards: [token]
 				}
-			}
+			};
+
+			sendRequest(options, res, callback, callback_error);
 		}
 
-		sendRequest(options, res, callback, callback_error);
-	}
-
-	var sendRequest = function(params, res, callback, callback_error)
-	{
-		var options =
+		var updateAccount = function(params, res, callback, callback_error)
 		{
-			host: 'api.conekta.io',
-			path: params.path,
-			method: params.method,
-			headers:
-			{
-				'Content-Type': 'application/json;charset=utf-8',
-				'Accept' : 'application/vnd.conekta-v1.0.0+json',
-				'Authorization': 'Basic ' + key
-			}
-		};
+			var conekta = params.conekta;
+			var card = params.card;
+			var token = params.token;
 
-		var request = https.request(options, function(response)
-		{
-			var content = "";
-			response.setEncoding('utf8');
-			response.on('data', function(part){ content += part; });
-			response.on('error', function(error)
+			var options =
 			{
-				logger.error(error);
-				if(callback_error) callback_error(error);
-				else errors(res, 'payment_error')
-			});
-			response.on('end', function()
-			{
-				var response = JSON.parse(content);
-
-				if(response.object == "error")
+				path: '/customers/' + conekta + '/cards/' + card,
+				method: 'PUT',
+				data:
 				{
-					logger.error(response.message);
+					token: token
+				}
+			}
 
-					if(callback_error)
+			sendRequest(options, res, callback, callback_error);
+		}
+
+		var pay = function(params, res, callback, callback_error)
+		{
+			var card = params.card;
+			var total = params.total;
+			var currency = params.currency;
+			var description = params.description;
+			var name = params.name;
+			var email = params.email;
+			var phone = params.phone;
+			var items = params.items;
+
+			var options =
+			{
+				path: '/charges',
+				method: 'POST',
+				data:
+				{
+					card: card,
+					amount: total,
+					currency: currency,
+					description: description,
+					details:
 					{
-						callback_error(response);
+						name: name,
+						email: email,
+						phone: phone,
+						line_items: items
 					}
-					else
+				}
+			}
+
+			sendRequest(options, res, callback, callback_error);
+		}
+
+		var sendRequest = function(params, res, callback, callback_error)
+		{
+			var options =
+			{
+				host: 'api.conekta.io',
+				path: params.path,
+				method: params.method,
+				headers:
+				{
+					'Content-Type': 'application/json;charset=utf-8',
+					'Accept' : 'application/vnd.conekta-v1.0.0+json',
+					'Authorization': 'Basic ' + key
+				}
+			};
+
+			var request = https.request(options, function(response)
+			{
+				var content = "";
+
+				response.setEncoding('utf8');
+				response.on('data', function(part){ content += part; });
+				response.on('error', function(error)
+				{
+					logger.error(error);
+					if(callback_error) callback_error(error);
+					else errors(res, 'payment_error')
+				});
+
+				response.on('end', function()
+				{
+					var response = JSON.parse(content);
+
+					if(response.object == "error")
 					{
-						if(response.type == "resource_not_found_error")
+						logger.error(response.message);
+
+						if(callback_error)
 						{
-							errors(res, 'payment_not_found');
-						}
-						else if(response.code)
-						{
-							if(response.code == "card_declined") errors(res, 'payment_card_declined');
-							else if(response.code == "expired_card") errors(res, 'payment_expired_card');
-							else if(response.code == "insufficient_funds") errors(res, 'payment_insufficient_funds');
-							else if(response.code == "suspected_fraud") errors(res, 'payment_suspected_fraud');
-							else if(response.code == "invalid_number") errors(res, 'payment_invalid_number');
-							else if(response.code == "invalid_expiry_month") errors(res, 'payment_invalid_expiry_month');
-							else if(response.code == "invalid_expiry_year") errors(res, 'payment_invalid_expiry_year');
-							else if(response.code == "invalid_cvc") errors(res, 'payment_invalid_cvc');
-							else if(response.code == "invalid_amount") errors(res, 'payment_invalid_amount');
-							else if(response.code == "invalid_payment_type") errors(res, 'payment_invalid_payment_type');
-							else if(response.code == "unsupported_currency") errors(res, 'payment_unsupported_currency');
-							else if(response.code == "missing_description") errors(res, 'payment_missing_description');
-							else if(response.code == "processing_error") errors(res, 'payment_processing_error');
-							else errors(res, 'payment_error');
+							callback_error(response);
 						}
 						else
 						{
-							errors(res, 'payment_error');
+							if(response.type == "resource_not_found_error")
+							{
+								errors(res, 'payment_not_found');
+							}
+							else if(response.code)
+							{
+								if(response.code == "card_declined") errors(res, 'payment_card_declined');
+								else if(response.code == "expired_card") errors(res, 'payment_expired_card');
+								else if(response.code == "insufficient_funds") errors(res, 'payment_insufficient_funds');
+								else if(response.code == "suspected_fraud") errors(res, 'payment_suspected_fraud');
+								else if(response.code == "invalid_number") errors(res, 'payment_invalid_number');
+								else if(response.code == "invalid_expiry_month") errors(res, 'payment_invalid_expiry_month');
+								else if(response.code == "invalid_expiry_year") errors(res, 'payment_invalid_expiry_year');
+								else if(response.code == "invalid_cvc") errors(res, 'payment_invalid_cvc');
+								else if(response.code == "invalid_amount") errors(res, 'payment_invalid_amount');
+								else if(response.code == "invalid_payment_type") errors(res, 'payment_invalid_payment_type');
+								else if(response.code == "unsupported_currency") errors(res, 'payment_unsupported_currency');
+								else if(response.code == "missing_description") errors(res, 'payment_missing_description');
+								else if(response.code == "processing_error") errors(res, 'payment_processing_error');
+								else errors(res, 'payment_error');
+							}
+							else
+							{
+								errors(res, 'payment_error');
+							}
 						}
+						return;
 					}
-					return;
-				}
 
-				callback(response);
+					callback(response);
+				});
 			});
-		});
 
-		request.write(JSON.stringify(params.data));
-		request.end();
+			request.write(JSON.stringify(params.data));
+			request.end();
+		}
+
+		component.createAccount = createAccount;
+		component.updateAccount = updateAccount;
+		component.pay = pay;
 	}
 
-	component.createAccount = createAccount;
-	component.updateAccount = updateAccount;
-	component.pay = pay;
+	if(version == 2)
+	{
+		var createAccount = function(params, res, callback, callback_error)
+		{
+			var data =
+			{
+				name: params.name,
+				email: params.email,
+				payment_sources:
+				[
+					{
+						token_id: params.token,
+						type: "card"
+					}
+				]
+			}
+
+			var options =
+			{
+				path: '/customers',
+				method: 'POST',
+				data: data
+			};
+
+			sendRequest(options, res, callback, callback_error);
+		}
+
+		var updateAccount = function(params, res, callback, callback_error)
+		{
+			var conekta = params.conekta;
+			var card = params.card;
+
+			var data =
+			{
+				token_id: params.token,
+				type: "card"
+			}
+
+			var options =
+			{
+				path: '/customers/' + conekta + '/payment_sources',
+				method: 'POST',
+				data: data
+			};
+
+			sendRequest(options, res, callback, callback_error);
+		}
+
+		var getAccount = function(params, res, callback, callback_error)
+		{
+			var conekta = params.conekta;
+
+			var options =
+			{
+				path: '/customers/' + conekta,
+				method: 'GET'
+			};
+
+			sendRequest(options, res, callback, callback_error);
+		}
+
+		var sendRequest = function(params, res, callback, callback_error)
+		{
+			var options =
+			{
+				host: 'api.conekta.io',
+				path: params.path,
+				method: params.method,
+				headers:
+				{
+					'Content-Type': 'application/json;charset=utf-8',
+					'Accept' : 'application/vnd.conekta-v2.0.0+json',
+					'Authorization': 'Basic ' + key
+				}
+			};
+
+			var request = https.request(options, function(response)
+			{
+				var content = "";
+
+				response.setEncoding('utf8');
+				response.on('data', function(part){ content += part; });
+				response.on('error', function(error)
+				{
+					logger.error(error);
+					if(callback_error) callback_error(error);
+					else errors(res, 'payment_error')
+				});
+
+				response.on('end', function()
+				{
+					var response = JSON.parse(content);
+
+					if(response.object == "error")
+					{
+						logger.error(response.message);
+
+						if(callback_error)
+						{
+							callback_error(response);
+						}
+						else
+						{
+							if(response.type == "resource_not_found_error")
+							{
+								errors(res, 'payment_not_found');
+							}
+							else if(response.code)
+							{
+								if(response.code == "card_declined") errors(res, 'payment_card_declined');
+								else if(response.code == "expired_card") errors(res, 'payment_expired_card');
+								else if(response.code == "insufficient_funds") errors(res, 'payment_insufficient_funds');
+								else if(response.code == "suspected_fraud") errors(res, 'payment_suspected_fraud');
+								else if(response.code == "invalid_number") errors(res, 'payment_invalid_number');
+								else if(response.code == "invalid_expiry_month") errors(res, 'payment_invalid_expiry_month');
+								else if(response.code == "invalid_expiry_year") errors(res, 'payment_invalid_expiry_year');
+								else if(response.code == "invalid_cvc") errors(res, 'payment_invalid_cvc');
+								else if(response.code == "invalid_amount") errors(res, 'payment_invalid_amount');
+								else if(response.code == "invalid_payment_type") errors(res, 'payment_invalid_payment_type');
+								else if(response.code == "unsupported_currency") errors(res, 'payment_unsupported_currency');
+								else if(response.code == "missing_description") errors(res, 'payment_missing_description');
+								else if(response.code == "processing_error") errors(res, 'payment_processing_error');
+								else errors(res, 'payment_error');
+							}
+							else
+							{
+								errors(res, 'payment_error');
+							}
+						}
+						return;
+					}
+
+					callback(response);
+				});
+			});
+
+			if(params.data) request.write(JSON.stringify(params.data));
+			request.end();
+		}
+
+		component.createAccount = createAccount;
+		component.updateAccount = updateAccount;
+		component.getAccount = getAccount;
+	}
 
 	return component;
 }
