1994ba3145e4d86481973173faff9903d81bf1f3
Marcos Martínez <socram61@gmail.com>
2018-01-17 13:48:32 -0600

    proceso de cargo a tarjeta

--- components/payment_openpay.js
+++ components/payment_openpay.js
@@ -5,6 +5,7 @@ function get(config, errors, logger)
 	var apikey = config.apikey;
 	var accountId = config.accountId;
 	var isProduction = config.production;
+	var currency = config.currency;
 
 
 	if(!apikey)
@@ -16,6 +17,12 @@ function get(config, errors, logger)
 	if(!accountId)
 	{
 		logger.error("openpay account id not found");
+		process.exit(1);
+	}
+
+	if(!currency)
+	{
+		logger.error("openpay currency not specified");
 		process.exit(1);
 	}
 
@@ -35,7 +42,7 @@ function get(config, errors, logger)
 	{
 		var customerRequest =
 		{
-			requires_account: false,
+			requires_account: params.requires_account,
 			name: params.name,
 			email: params.email
 		};
@@ -137,12 +144,52 @@ function get(config, errors, logger)
 		});
 	}
 
+	var processPaymentByUser = function (params, res, callback, callback_error){
+
+		var chargeRequest = {
+		   'source_id' : params.sourceId,
+		   'method' : params.method,
+		   'amount' : params.amount,
+		   'currency' : currency,
+		   'description' : params.description,
+		   'device_session_id' : params.deviceId,
+		}
+
+		if(params.orderId) { chargeRequest.order_id = params.orderId; }
+
+		if(params.method == 'card') { chargeRequest.cvv2 = params.cvv; }
+
+		if(!params.capture) { chargeRequest.capture = false; } // preautorize
+
+		openpay.charges.create(params.customerId, chargeRequest, function(error, charge) {
+
+			if (error) {
+				if(callback_error) { // llamamos callback de error o respondemos
+					callback_error(error);
+				} else {
+
+					var response = {
+						code: error.http_code,
+						msg: error.description + ' ( ' + error.error_code + ' )'
+					};
+
+					if(res) res.status(error.http_code).send(response);
+				}
+				return error;
+			}
+
+			callback(charge) // exito devolvemos array de cards
+
+		});
+	}
+
 
 
 	component.createAccount = createAccount;
 	component.saveCard = saveCard;
 	component.listCardsByUser = listCardsByUser;
 	component.deleteCard = deleteCard;
+	component.processPaymentByUser = processPaymentByUser;
 
 	return component;
 }
